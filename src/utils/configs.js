4cf538903f6811833811b552d5eafd47d3a521d3
Stet <steterik@gmail.com>
2023-03-09 10:33:50 +0600

    server url is changed

--- src/utils/configs.js
+++ src/utils/configs.js
@@ -1,8 +1,14 @@
 const isLocalhost = Boolean(
 	window.location.hostname === 'localhost' ||
 		window.location.hostname === '[::1]' ||
-		window.location.hostname.match(/^127(?:\.(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)){3}$/)
+		window.location.hostname.match(
+			/^127(?:\.(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)){3}$/
+		)
 )
 
-export const SERVER_URL = isLocalhost ? 'http://localhost:3030/' : 'https://protected-coast-49560.herokuapp.com/'
-export const SSE = new EventSource(`${SERVER_URL}stream`, { withCredentials: true })
+export const SERVER_URL = isLocalhost
+	? 'http://localhost:3030/'
+	: 'https://oyster-hat.cyclic.app/'
+export const SSE = new EventSource(`${SERVER_URL}stream`, {
+	withCredentials: true
+})
